0a9969a3af32516e25db7bc84b9d092b59157313
Lucy Yip <lyip@risd.edu>
2019-08-15 01:36:29 -0700

    /

--- sketch.js
+++ sketch.js
@@ -1,20 +1,22 @@
 var mclick = 0;
-var timestamp;
+var ts;
 var stage = 1;
 var SGB;
 var SGR;
 var trigger = false;
+var welc;
+var counter = 0;
 // var loadcube;
 
 function preload() {
   SGB = loadFont('font/SegoeUIBold.ttf');
   SGR = loadFont('font/SegoeUI.ttf');
-  var welco = loadImage('img/load.jpg');
+  welc = loadImage('img/load.jpg');
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-
+  imageMode(CENTER);
 }
 
 function draw() {
@@ -23,9 +25,13 @@ function draw() {
   var locX = mouseX ;
   var locY = mouseY;
 
+
+// -------------------------------------------------------------------------------------------------------------------------------------- started block
+
+
   if(stage==1){
-    timestamp = frameCount;
-    stage = 3;
+    counter=0;
+    stage = 2;
   }
   
   
@@ -34,6 +40,7 @@ function draw() {
   
   
   if(stage==2){
+  counter++;
   background(0);
   noStroke();
   
@@ -41,44 +48,82 @@ function draw() {
   fill('#648493');
   rect(-200,20,400,2);
   fill(0,174,255);
-  rect(-200,20,frameCount-timestamp,2);
+  rect(-200,20,counter,2);
   
   fill(255);
   textFont(SGB);
   textSize(12);
   textAlign(CENTER);
-  text((frameCount-timestamp)/4-0.25+" %",0,0);
+  text(counter/4-0.25+" %",0,0);
   
-  
-  if(frameCount-timestamp > 400){
-    timestamp = frameCount;
+  if(counter > 400){
+    counter = 0;
     stage = 3;
   }
   pop()
+  
   }
 
 
 // -------------------------------------------------------------------------------------------------------------------------------------- facade system welcome page and logo
 
   if(stage==3){
-    push();
+  counter++;
+  push();
   background(255);
-  imageMode(CENTER);
-  if(width*0.56<height){
-    image(welc,0,0,height/0.56,height);
-  }else{
-    image(welc,0,0,width,width*0.56);
-  }
+  image(welc,0,-50,300,300);
+
   rectMode(CENTER);
-  fill(0,0,0,255-frameCount/3);
+  fill(0,0,0,255-counter);
   rect(0,0,2*width,2*height);
   pop();
-
-}
-}
-
-function mouseClicked() {
-  if(trigger ==true){
-  mclick++;
+  
+  noStroke();
+  fill(0,0,0,counter/2);
+  textSize(24);
+  textAlign(CENTER);
+  textFont(SGB);
+  text("FACADE",0,150);
+  textSize(16);
+  textFont(SGR);
+  text("Microsoft Tenant Data Physicalization Tool",0,180);
+  
+  if(counter < 300){
+  stroke(0,0,0,counter/2);
+  strokeWeight(2);
+  line(-counter/2,160,counter/2,160);
   }
-}
+  if(counter > 300){
+  stroke(0,0,0,counter/2);
+  strokeWeight(2);
+  line(-150,160,150,160);
+  noStroke();
+  }
+  
+  if(counter > 500){
+    fill(255,255,255,(counter-500)*5)
+    rectMode(CENTER);
+    rect(0,0,width/2,height/2);
+  }
+  
+  if(counter>530){
+  stage = 4;
+  counter=0;
+  }
+// -------------------------------------------------------------------------------------------------------------------------------------- facade system welcome page and logo
+
+  if(stage==4){
+    
+  counter++;
+  
+  }
+
+
+
+}}
+
+// function mouseClicked() {
+//   if(trigger ==true){
+//   mclick++;
+//   }
+// }
